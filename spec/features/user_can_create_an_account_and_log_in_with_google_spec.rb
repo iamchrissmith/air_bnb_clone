6a4a41239c2b8e3620c187966169f919cdee768f
Beth Knight <bethknight1234@gmail.com>
2017-05-18 11:15:00 -0600

    Feature test google signup

--- spec/features/user_can_create_an_account_and_log_in_with_google_spec.rb
+++ spec/features/user_can_create_an_account_and_log_in_with_google_spec.rb
@@ -4,13 +4,28 @@ RSpec.feature "User can authenticate with Google by" do
 
   scenario "creating an account with Google" do
 
-    visit log_in_path
+    visit root_path
 
+    click_on "Sign Up"
+
+    expect(current_path).to eq(sign_up_path)
+
+    click_on "Sign up with Google"
+
+    expect(current_path).to eq(edit_profile_path)
+    expect(page.body).to have_content("First name: Joe")
+    expect(page.body).to have_content("Last name: Schmoe")
+    expect(page.body).to have_content("Email address: JoeSchmoe123@gmail.com")
+    expect(page.body).to have_link("Log out")
   end
 
-  scenario "logging in with Google" do
+  xscenario "logging in with Google" do
 
+    visit login_path
 
+    click_on "Log in with Google"
+
+    expect(current_path).to eq(dashboard_path)
   end
 
 end

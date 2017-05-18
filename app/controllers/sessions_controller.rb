e67af8657c4caa40ed60a535ee29b04e3ad8d42c
slague <stephanielague@gmail.com>
2017-05-18 15:16:31 -0600

    Remove bye bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-byebug
+
     if user = User.from_fb_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
-      byebug
       if current_user.phone_number.nil?
         redirect_to edit_user_path(user)
       else

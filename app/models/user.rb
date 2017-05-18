2b0d0fe6c140c1606a0a5b03c9f44c253b897774
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-05-18 12:04:43 -0600

    add devise views and functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :reservations, foreign_key: "renter_id"
   has_many :properties, foreign_key: "owner_id"
 
   enum role: %w(registered_user admin)
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
 
+  get '/sign_up', to: 'users#new'
+  get '/log_in', to: 'sessions#new'
   get '/auth/facebook', as: :facebook_login
   get '/auth/google_oauth2', as: :google_login
   get '/auth/:provider/callback', to: 'sessions#create'

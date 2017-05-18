1f52e7b09503d6dcaec615658eb8e31dfe3a16e4
somedayrainbows <ebassity@gmail.com>
2017-05-18 13:07:00 -0600

    Sets up feature test for guest login

---
+++ spec/features/guest_login_with_email_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "guest can login with email" do
+  scenario "guest can register" do
+
+  end
+
+  scenario "guest can login as new registered user" do
+
+  end
+end

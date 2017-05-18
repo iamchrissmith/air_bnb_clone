ff559735b39b498555a4cd56b6c16349deb4e6d4
Beth Knight <bethknight1234@gmail.com>
2017-05-17 18:21:01 -0600

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+
+  def initialize
+    generate_users
+    generate_properties
+  end
+
+  def generate_users
+    1000.times do |i|
+      user = User.create!(email: Faker::Internet.email,
+                          first_name: ,
+                          last_name: ,
+                          image_url: ,
+                          phone_number:
+                          description: ,
+                          hometown: ,
+                          role: ,
+                          active?:
+
+      )
+  end
+
+
+  def generate_properties
+    500.times do |i|
+      property = Property.create!(name: ,
+                                  number_of_guests: ,
+                                  number_of_beds: ,
+                                  number_of_rooms: ,
+                                  description: ,
+                                  price_per_night: ,
+                                  address: ,
+                                  city: ,
+                                  state: ,
+                                  zip: ,
+                                  lat: ,
+                                  lat: ,
+                                  long: ,
+                                  image_url: ,
+                                  check_in_time: ,
+                                  check_out_time: ,
+                                  status:
+
+      )
+  end
+
+end
+
+Seed.new

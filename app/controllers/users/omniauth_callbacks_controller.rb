d29c50240d6b7e72576c89e196ab4b8217effa8a
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-05-20 12:15:51 -0600

    confirmed local host login still works

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.phone_number.nil?
         redirect_to edit_user_path(@user)
       else
+
         redirect_to dashboard_path
       end
     else
@@ -18,7 +19,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
+
 
     if @user.persisted?
       sign_in @user, :event => :authentication
@@ -26,6 +29,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.phone_number.nil?
         redirect_to edit_user_path(@user)
       else
+
         redirect_to dashboard_path
       end
     else

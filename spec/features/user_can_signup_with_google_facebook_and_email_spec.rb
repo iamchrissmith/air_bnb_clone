ebd9ec193233ed2c928b17a8790ed352075995cb
VictoriaVasys <vvasys@gmail.com>
2017-05-20 13:08:28 -0600

    Add sign-up with email to multiple signups test

--- spec/features/user_can_signup_with_google_facebook_and_email_spec.rb
+++ spec/features/user_can_signup_with_google_facebook_and_email_spec.rb
@@ -4,6 +4,20 @@ feature "User can sign up with facebook, google, and email" do
   let(:already_created_user) {create(:user)}
   
   scenario "user can signup with same email for facebook and google" do
+    visit sign_up_path
+    click_on "Sign Up with Email"
+
+    expect(current_path).to eq(new_user_registration_path)
+    fill_in :user_first_name, with: "Erin"
+    fill_in :user_last_name, with: "B"
+    fill_in :user_email, with: "erin@erin.net"
+    fill_in :user_phone_number, with: "123-456-7899"
+    fill_in :user_image_url, with: "https://fakeimage.jpg"
+    fill_in :user_password, with: "password"
+    fill_in :user_password_confirmation, with: "password"
+    click_on "Sign up"
+    click_on "Log Out"
+    
     stub_facebook
     already_created_user.update(phone_number: nil)
     allow(User).to receive(:from_omniauth).and_return(already_created_user)
@@ -16,7 +30,7 @@ feature "User can sign up with facebook, google, and email" do
     fill_in "Hometown", with: 'STL'
     
     click_on "Update Profile"
-    click_on('Log Out')
+    click_on "Log Out"
     
     stub_google
     allow(User).to receive(:from_omniauth).and_return(User.last)

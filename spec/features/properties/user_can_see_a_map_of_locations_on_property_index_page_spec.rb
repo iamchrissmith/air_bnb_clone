23ce9da37f5a2ec9442c30d5f37e098fa19ebb62
Beth Knight <bethknight1234@gmail.com>
2017-05-24 15:17:52 -0600

    property index test

--- spec/features/properties/user_can_see_a_map_of_locations_on_property_index_page_spec.rb
+++ spec/features/properties/user_can_see_a_map_of_locations_on_property_index_page_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 feature "user can view map of locations on a property index page" do
   context "as a guest user" do
-
     before do
-      @property_1 = create(:property, name: "Beth's Plac", address: "816 Acoma St", city: "Denver", state: "CO", zip: "80203")
+      @property_1 = create(:property, name: "Beth's Place", address: "816 Acoma St", city: "Denver", state: "CO", zip: "80203")
       @property_2 = create(:property, name: "Crossfit Broadway", address: "1025 Acoma St", city: "Denver", state: "CO", zip: "80203")
       @property_3 = create(:property, name: "Lowdown", address: "800 Lincoln St", city: "Denver", state: "CO", zip: "80203")
       @key = ENV['GOOGLE_MAP_KEY']
@@ -14,11 +13,12 @@ feature "user can view map of locations on a property index page" do
 
       visit properties_path
 
-      expect(page).to have_css('iframe#map-index')
+      within(".index_map") do
 
-      expect(page).to have_css("iframe[src='https://www.google.com/maps/embed/v1/place?key=#{@key}&q=#{@property_1.prepare_address}']")
-      expect(page).to have_css("iframe[src='https://www.google.com/maps/embed/v1/place?key=#{@key}&q=#{@property_2.prepare_address}']")
-      expect(page).to have_css("iframe[src='https://www.google.com/maps/embed/v1/place?key=#{@key}&q=#{@property_3.prepare_address}']")
+      iframe = page.find('iframe')
+      
+      expect(iframe[:src]).to eq("https://www.google.com/maps/embed/v1/place?key=#{@key}&q=[\"#{@property_1.prepare_address}\", \"#{@property_2.prepare_address}\", \"#{@property_3.prepare_address}\"]")
+      end
     end
   end
 end

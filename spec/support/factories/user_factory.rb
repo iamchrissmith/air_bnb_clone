328d2f509af5ecf2b30853ca9d57d9f72dd74992
Aaron Olsen <mraaronolsen@gmail.com>
2017-07-27 11:47:36 -0600

    Faker completely removed from factories

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name 'User'
     last_name 'Mc Userface'
-    image_url Faker::Fillmurray.image
+    image_url 'http://www.picture.com'
     sequence(:email) { |n| "user#{n}@gmail.com" }
     phone_number '123-456-7890'
     description 'Im a user with a Murry face'

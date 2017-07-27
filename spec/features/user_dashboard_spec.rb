6d6ff8e6e8a1dbc44d3b80f3cc494188a0a07f74
Chris Smith <42@chrisryansmith.com>
2017-07-27 09:57:36 -0600

    add pending reservation scenario to user_dashboard spec

--- spec/features/user_dashboard_spec.rb
+++ spec/features/user_dashboard_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 feature "as a logged in user" do
   context "when I visit my dashboard" do
+    let(:user) { create(:user) }
     scenario "I see my name, photo, and links to view and edit my profile" do
-
-      user = create(:user)
       login(user)
 
       visit dashboard_path
@@ -13,13 +12,25 @@ feature "as a logged in user" do
       expect(page).to have_content(user.first_name)
       expect(page).to have_link('View Profile')
       expect(page).to have_link('Edit Profile')
+    end
+    scenario "I see my pending reservations" do
+      reservation = create(:reservation, renter: user, status: 0)
+      login(user)
+
+      visit dashboard_path
+
+      expect(page).to have_content("Your Pending Reservations")
+      expect(page).to have_css('.reservations .nav-tabs a[data-toggle="pending"]')
+      expect(page).to have_css('.reservations .tab-content #pending')
+      within ('.reservations .tab-content #pending') do
+        expect(page).to have_content reservation.start_date
+        expect(page).to have_content reservation.end_date
+        expect(page).to have_link(reservation.property.name, href: property_path(reservation.property))
+        expect(page).to have_content "Requested on: #{reservation.created_at}"
       end
     end
-
-
-
-#Need to figure out messages...
-
+  end
+  #Need to figure out messages...
   context "Within my notifications" do
 
     xscenario "I see the number of new messages with a link to messages" do

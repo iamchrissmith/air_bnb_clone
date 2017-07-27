722cb9b95e05c30bd070230f023ec21b470865fe
Chris Smith <42@chrisryansmith.com>
2017-07-27 09:27:32 -0600

    add date validations to reservation submit ajax

--- app/assets/javascripts/reservations/submit.js
+++ app/assets/javascripts/reservations/submit.js
@@ -1,4 +1,21 @@
-$().ready(function() {
+$(document).ready(function() {
+
+  jQuery.validator.addMethod("greaterThan",
+  function(value, element, params) {
+    if (params == 'today') {
+      compareTo = new Date();
+    } else {
+      compareTo = $(params).val();
+    }
+    console.log("Value: "+value);
+    console.log("Compare: "+compareTo);
+    if (!/Invalid|NaN/.test(new Date(value))) {
+        return new Date(value) > new Date(compareTo);
+    }
+
+    return isNaN(value) && isNaN(compareTo) || (Number(value) > Number(compareTo));
+  },'Must be greater than {0}.');
+
   function successfulReservation (response) {
     $('.booking-box form').remove();
     $('#reservation-success-message').show();
@@ -23,4 +40,18 @@ $().ready(function() {
       });
     }
   });
-})
+  $('#reservation_start_date').rules("add", {
+    required: true,
+    greaterThan: 'today',
+    messages: {
+      greaterThan: "Start Date must be after today"
+    }
+  });
+  $('#reservation_end_date').rules("add", {
+    required: true,
+    greaterThan: '#reservation_start_date',
+    messages: {
+      greaterThan: "End Date must be after Start Date"
+    }
+  });
+});

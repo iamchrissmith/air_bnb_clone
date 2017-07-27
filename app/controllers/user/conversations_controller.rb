31457e5cb6e81cb8dc15231bc6a8bf4974ed074a
rongxanh88 <rongxanh88@gmail.com>
2017-07-27 11:44:47 -0600

    add more html and add coffee script for conversations

--- app/controllers/user/conversations_controller.rb
+++ app/controllers/user/conversations_controller.rb
@@ -1,9 +1,32 @@
 class User::ConversationsController < ApplicationController
   def index
-
+    if current_user
+      @conversations = current_user.conversations
+    else
+      redirect_to '/'
+    end
   end
 
   def show
-    @conversation = Converation.includes(:messages).find(params[:id])
+    @conversation = current_user.conversations.includes(:messages).find(params[:id])
+    # @conversation = Converation.includes(:messages).find(params[:id])
   end
+
+  def new
+    
+  end
+
+  def create
+    @conversation = current_user.conversations.build(conversation_params)
+    if @conversation.save
+      redirect_to user_conversations_path
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+    def conversation_params
+      params.require(:conversation).permit(:title)
+    end
 end

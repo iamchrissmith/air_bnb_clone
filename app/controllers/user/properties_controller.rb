4d7ba0f17276bfdf60b7011cbed31d4a272d2b29
Chris Smith <42@chrisryansmith.com>
2017-07-27 14:37:31 -0600

    reseting user/properties index to just display data about properties so we can make a user/property/show page with more details about requests

--- app/controllers/user/properties_controller.rb
+++ app/controllers/user/properties_controller.rb
@@ -2,6 +2,7 @@ class User::PropertiesController < ApplicationController
 
   def index
     @properties = current_user.properties
+    # binding.pry
   end
 
 end
--- spec/features/reservations/owner_can_view_reservations_spec.rb
+++ spec/features/reservations/owner_can_view_reservations_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
-RSpec.feature 'Owner can view requested requests' do
+RSpec.feature 'Owner can view properties index' do
   context 'when I visit my properties' do
     let(:owner) { create(:user) }
     let(:renter) { create(:user) }
-    let(:property) { create(:property, owner: owner) }
+    let(:property1) { create(:property, owner: owner) }
+    let(:property2) { create(:property, owner: owner) }
     let!(:pending_request) { create(:reservation,
-                                          property: property,
+                                          property: property1,
                                           renter: renter,
                                           status: 0,
                                           start_date: Date.today + 10,
                                           end_date: Date.today + 13) }
     let!(:confirmed_request) { create(:reservation,
-                                          property: property,
+                                          property: property1,
                                           renter: renter,
                                           status: 1,
                                           start_date: Date.today + 4,
                                           end_date: Date.today + 5) }
     let!(:in_progress_request) { create(:reservation,
-                                          property: property,
+                                          property: property1,
                                           renter: renter,
                                           status: 2,
                                           start_date: Date.today - 1,
                                           end_date: Date.today + 3) }
     let!(:finished_request) { create(:reservation,
-                                          property: property,
+                                          property: property2,
                                           renter: renter,
                                           status: 3) }
     let!(:declined_request) { create(:reservation,
-                                          property: property,
+                                          property: property2,
                                           renter: renter,
                                           status: 4) }
 
-    scenario 'owner can see pending requests' do
+    scenario 'owner can see properties with summary of requests' do
       login(owner)
 
       visit user_properties_path
@@ -46,74 +47,6 @@ RSpec.feature 'Owner can view requested requests' do
         expect(page).not_to have_css("#request-#{in_progress_request.id}")
         expect(page).not_to have_css("#request-#{finished_request.id}")
         expect(page).not_to have_css("#request-#{declined_request.id}")
-      end
-    end
-
-    scenario 'owner can see confirmed requests' do
-      login(owner)
-
-      visit user_properties_path
-
-      expect(page).to have_content "Confirmed Requests"
-      expect(page).to have_css('.nav-tabs a[href="#confirmed"]')
-      expect(page).to have_css('.tab-content #confirmed')
-      within ('.tab-content #confirmed') do
-        expect(page).not_to have_css("#request-#{pending_request.id}")
-        expect(page).to have_css("#request-#{confirmed_request.id}")
-        expect(page).not_to have_css("#request-#{in_progress_request.id}")
-        expect(page).not_to have_css("#request-#{finished_request.id}")
-        expect(page).not_to have_css("#request-#{declined_request.id}")
-      end
-    end
-
-    scenario 'owner can see in_progress requests' do
-      login(owner)
-
-      visit user_properties_path
-
-      expect(page).to have_content "In Progress Requests"
-      expect(page).to have_css('.nav-tabs a[href="#in_progress"]')
-      expect(page).to have_css('.tab-content #in_progress')
-      within ('.tab-content #in_progress') do
-        expect(page).not_to have_css("#request-#{pending_request.id}")
-        expect(page).not_to have_css("#request-#{confirmed_request.id}")
-        expect(page).to have_css("#request-#{in_progress_request.id}")
-        expect(page).not_to have_css("#request-#{finished_request.id}")
-        expect(page).not_to have_css("#request-#{declined_request.id}")
-      end
-    end
-
-    scenario 'owner can see finished requests' do
-      login(owner)
-
-      visit user_properties_path
-
-      expect(page).to have_content "Finished Requests"
-      expect(page).to have_css('.nav-tabs a[href="#finished"]')
-      expect(page).to have_css('.tab-content #finished')
-      within ('.tab-content #finished') do
-        expect(page).not_to have_css("#request-#{pending_request.id}")
-        expect(page).not_to have_css("#request-#{confirmed_request.id}")
-        expect(page).not_to have_css("#request-#{in_progress_request.id}")
-        expect(page).to have_css("#request-#{finished_request.id}")
-        expect(page).not_to have_css("#request-#{declined_request.id}")
-      end
-    end
-
-    scenario 'owner can see declined requests' do
-      login(owner)
-
-      visit user_properties_path
-
-      expect(page).to have_content "Declined Requests"
-      expect(page).to have_css('.nav-tabs a[href="#declined"]')
-      expect(page).to have_css('.tab-content #declined')
-      within ('.tab-content #declined') do
-        expect(page).not_to have_css("#request-#{pending_request.id}")
-        expect(page).not_to have_css("#request-#{confirmed_request.id}")
-        expect(page).not_to have_css("#request-#{in_progress_request.id}")
-        expect(page).not_to have_css("#request-#{finished_request.id}")
-        expect(page).to have_css("#request-#{declined_request.id}")
       end
     end
   end

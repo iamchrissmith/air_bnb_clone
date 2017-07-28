fe485c6b3fb3483f42f16cea06c6b16252bb5015
Chris Smith <42@chrisryansmith.com>
2017-07-28 08:50:31 -0600

    setup request partials to display necessary info

--- spec/features/properties/owner_can_view_property_show_spec.rb
+++ spec/features/properties/owner_can_view_property_show_spec.rb
@@ -41,19 +41,28 @@ RSpec.feature 'Owner can view properties index' do
 
       expect(page).to have_css('#in-progress-requests')
       within ('#in-progress-requests') do
-        expect(page).to have_css("#request-#{pending_request.id}")
+        expect(page).to have_css("#request-#{in_progress_request.id}")
+        expect(page).to have_content in_progress_request.start_date.to_formatted_s(:short)
+        expect(page).to have_content in_progress_request.end_date.to_formatted_s(:short)
+        expect(page).to have_link in_progress_request.renter.full_name, href: user_path(in_progress_request.renter)
+        expect(page).to have_content "$#{in_progress_request.total_price}"
+        expect(page).to have_content "Message User"
       end
       expect(page).to have_css('#pending-requests')
       within ('#pending-requests') do
-        expect(page).to have_css("#request-#{confirmed_request.id}")
+        expect(page).to have_css("#request-#{pending_request.id}")
+        expect(page).to have_content "Approve Request"
+        expect(page).to have_content "Message User"
       end
       expect(page).to have_css('#confirmed-requests')
       within ('#confirmed-requests') do
-        expect(page).to have_css("#request-#{in_progress_request.id}")
+        expect(page).to have_css("#request-#{confirmed_request.id}")
+        expect(page).to have_content "Message User"
       end
       expect(page).to have_css('#finished-requests')
       within ('#finished-requests') do
         expect(page).to have_css("#request-#{finished_request.id}")
+        expect(page).to have_content "Review User"
       end
       expect(page).to have_css('#declined-requests')
       within ('#declined-requests') do

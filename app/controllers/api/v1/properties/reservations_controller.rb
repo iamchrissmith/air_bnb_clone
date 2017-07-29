978165b5e891ec1e93123cfa9288f61d5d493999
rongxanh88 <rongxanh88@gmail.com>
2017-07-29 17:30:47 -0600

    trip conversations now created after reservation is made

--- app/controllers/api/v1/properties/reservations_controller.rb
+++ app/controllers/api/v1/properties/reservations_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::Properties::ReservationsController < ApiController
     reservation = @property.reservations.new(reservation_params)
     if reservation.save
       render json: {success:"Reservation Created", reservation: reservation}, status: 201
+      create_trip_messaging
     else
       render json: {error: "Missing Parameters"}, status: 401
     end
@@ -24,5 +25,15 @@ class Api::V1::Properties::ReservationsController < ApiController
 
     def get_property
       @property = Property.find(params[:property_id])
+    end
+
+    def create_trip_messaging
+      author = User.find(params[:reservation][:renter_id])
+      receiver = User.joins(:properties)
+                 .joins("INNER JOIN reservations ON reservations.property_id = properties.id")
+                 .where("reservations.property_id = ?", params[:property_id]).first
+      property = Property.joins(:reservations).find(params[:property_id])
+      
+      Conversation.new(title: "Trip to #{property.name}.", author_id: author.id, receiver_id: receiver.id)
     end
 end

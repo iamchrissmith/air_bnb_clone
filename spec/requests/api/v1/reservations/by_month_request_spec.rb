220180fd9e00d31f0d59256dc02af47c0a0e81f7
Chris Smith <42@chrisryansmith.com>
2017-07-31 16:01:12 -0600

    add timecop to control for by_month api endpoint irregularities

--- spec/requests/api/v1/reservations/by_month_request_spec.rb
+++ spec/requests/api/v1/reservations/by_month_request_spec.rb
@@ -2,44 +2,48 @@ require 'rails_helper'
 
 describe 'Reservations by month' do
   it 'returns number of reservations for each month' do
+    Timecop.freeze(Time.local(2017, 7, 1, 10, 00, 00)) do
+      reservation1 = create(:reservation, start_date: Date.today)
+      reservation2 = create(:reservation, start_date: Date.today+1.month)
+      reservation3 = create(:reservation, start_date: Date.today+3)
+      reservation4 = create(:reservation, start_date: Date.tomorrow)
 
-    reservation1 = create(:reservation, start_date: Date.today)
-    reservation2 = create(:reservation, start_date: Date.today+30)
-    reservation3 = create(:reservation, start_date: Date.yesterday)
-    reservation4 = create(:reservation, start_date: Date.tomorrow)
+      get "/api/v1/reservations/by_month"
 
-    get "/api/v1/reservations/by_month"
+      expect(response).to be_success
+      reservations = JSON.parse(response.body)
 
-    expect(response).to be_success
-    reservations = JSON.parse(response.body)
+      next_month = (Date.today+1.month).strftime("%B")
 
-    first_month = (Date.today+30).strftime("%B")
-
-    expect(reservations.count).to eq(2)
-    expect(reservations.first['month']).to eq(first_month)
-    expect(reservations.first['count']).to eq(1)
+      expect(reservations.count).to eq(2)
+      expect(reservations.first['month']).to eq(next_month)
+      expect(reservations.first['count']).to eq(1)
+    end
   end
 
   it 'returns number of reservations for each month for a city' do
-    property1 = create(:property, city: 'Denver')
-    property2 = create(:property, city: 'San Francisco')
-    reservation1 = create(:reservation, property_id: property1.id, start_date: Date.today)
-    reservation2 = create(:reservation, property_id: property1.id, start_date: Date.today+30)
-    reservation3 = create(:reservation, property_id: property2.id, start_date: Date.yesterday)
-    reservation4 = create(:reservation, property_id: property1.id, start_date: Date.tomorrow)
+    Timecop.freeze(Time.local(2017, 7, 1, 10, 00, 00)) do
+      property1 = create(:property, city: 'Denver')
+      property2 = create(:property, city: 'San Francisco')
+      reservation_p1_this_month = create(:reservation, property_id: property1.id, start_date: Date.today)
+      reservation_p1_next_month = create(:reservation, property_id: property1.id, start_date: Date.today+1.month)
+      reservation_p1_this_month2 = create(:reservation, property_id: property1.id, start_date: Date.tomorrow)
 
-    get "/api/v1/reservations/by_month?city=Denver"
+      other_property_reservation = create(:reservation, property_id: property2.id, start_date: Date.yesterday)
 
-    expect(response).to be_success
-    reservations = JSON.parse(response.body)
+      get "/api/v1/reservations/by_month?city=Denver"
 
-    first_month = (Date.today+30).strftime("%B")
-    second_month = Date.today.strftime("%B")
+      expect(response).to be_success
+      reservations = JSON.parse(response.body)
 
-    expect(reservations.count).to eq(2)
-    expect(reservations.first['month']).to eq(first_month)
-    expect(reservations.first['count']).to eq(1)
-    expect(reservations.second['month']).to eq(second_month)
-    expect(reservations.second['count']).to eq(2)
+      next_month = (Date.today+1.month).strftime("%B")
+      this_month = Date.today.strftime("%B")
+
+      expect(reservations.count).to eq(2)
+      expect(reservations.first['month']).to eq(next_month)
+      expect(reservations.first['count']).to eq(1)
+      expect(reservations.second['month']).to eq(this_month)
+      expect(reservations.second['count']).to eq(2)
+    end
   end
 end
